9657532f24c14f93092f3e435d3edaceedde0a16
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-07 18:27:25 -0700

    basic styling, hard coded state with data, passed down data as properties to Todos component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,32 @@ import React from 'react';
 import './App.css';
 import Todos from './components/Todos';
 class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Study',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with husbando Viktor',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Work out for snatched body',
+        completed: false
+      }
+    ]
+  }
+
   render () {
+    console.log(this.state.todos);
+
     return (
     <div className="App">
-     <h1>app</h1>
+     <Todos todos={this.state.todos} />
     </div>
    );
   }
